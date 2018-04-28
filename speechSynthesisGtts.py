2efd46ceaa2ceaf03ae29f053b00ad2f289ce6e6
pythonpsycho1337 <root@LAPTOP-V8QOMECQ.localdomain>
2018-04-28 17:13:37 +0200

    speechSynthesisScriptDone :D

--- speechSynthesisGtts.py
+++ speechSynthesisGtts.py
@@ -1,5 +1,32 @@
 from gtts import gTTS
+import os
+import time
 
-tts = gTTS(text='Clement, welcome to sweden', lang='en')
-filename = 'temp.mp3'
-tts.save(filename)
+path = "../VCTK/VCTK-Corpus/txt/"
+
+dirs = os.listdir(path)
+start_time = time.time()
+for diri in range(0,len(dirs)):
+    filenames = os.listdir(path+dirs[diri])
+    sentences = []
+
+    if not os.path.exists("output/"+dirs[diri]):
+        os.makedirs("output/"+dirs[diri])
+
+    for filename in filenames:
+        f = open(path+dirs[diri]+"/"+filename,"r")
+        sentences.append(f.readline())
+
+    tts = []
+    for i in range(0,len(sentences)):
+        tts.append(gTTS(text=sentences[i], lang="en"))
+
+    for i in range(0,len(sentences)):
+        tts[i].save("output/"+dirs[diri]+"/"+filenames[i][:-3]+"mp3")
+
+    print("[" + ('%.1f' % round(100 * (diri + 1) / len(dirs), 2)) + "%] directory " + dirs[diri] + " done")
+    timeElapsed = (time.time() - start_time)
+    timeLeft = ((timeElapsed) / (diri + 1)) * (len(dirs) - diri - 1)
+    print("Time elapsed: %s seconds" % (int(timeElapsed)))
+    print("Time left: %s seconds" % (int(timeLeft)))
+
