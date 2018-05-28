f88c0640eafe697d2e568a897f9c635bddff4ff1
yonk <nopctoday@gmail.com>
2018-05-28 18:25:13 +0200

    debugging loss 0 issue

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ def main():
     print('model built')
     mean_loss = tf.reduce_mean(loss)
     loss_summary_op = tf.summary.scalar('loss', mean_loss, family='loss and accuracy')
-    accuracy_op = calc_accuracy(labels,wavenet_out, seq_length)
+    accuracy_op, predicted_out = calc_accuracy(labels,wavenet_out, seq_length)
     accuracy_summary_op = tf.summary.scalar('accuracy', accuracy_op, family='loss and accuracy')
 
     summary_op = tf.summary.merge([accuracy_summary_op, loss_summary_op])
@@ -36,6 +36,7 @@ def main():
 
     # DEBUGING STUFF
     densified_label = tf.sparse_tensor_to_dense(labels)
+    dense_predicted = tf.sparse_tensor_to_dense(predicted_out[0])
 
     with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
         sess.run(tf.global_variables_initializer())
@@ -45,16 +46,20 @@ def main():
 
             if step % 10 == 0:
                 #a,b,c,d, = sess.run([labels]) debugging pipeline output
-                _, loss_out, accuracy_out,summary, _x_out_, _wavenet_out_, _label_text_, _densified_label_, _seq_len, _x_file_name = sess.run([opt_op, loss, accuracy_op, summary_op, x, wavenet_out, label_text, densified_label, seq_length, x_file_name])
+                _, loss_out, accuracy_out,summary, _x_out_, _wavenet_out_, _label_text_, _densified_label_, _seq_len, _x_file_name, _predicted_out = sess.run([opt_op, loss, accuracy_op, summary_op, x, wavenet_out, label_text, densified_label, seq_length, x_file_name, dense_predicted])
                 print('step',step,'loss', np.mean(loss_out))
                 if np.mean(loss_out) < 1:
                     #print(x)
                     #print(loss_out)
                     #idx = _label_text_[0]
+
                     filename = _x_file_name[0].decode('utf-8')
                     label_idx = np.fromstring(_label_text_[0], np.int64)
                     label = index2str(label_idx)
+                    predicted = index2str(_predicted_out[0])
+
                     print(label)
+                    print(predicted)
 
                     #print(_wavenet_out_)
 
@@ -74,11 +79,11 @@ def grad_tower(opt, labels, x, seq_length):
         wavenet_out, wavenet_no_softmax = build_wavenet(x, voca_size=conf.ALPHA_SIZE)
         loss = tf.nn.ctc_loss(labels, wavenet_no_softmax, seq_length,
                               time_major=False, # batch x time x alpha_dimgt
-                              # ctc_merge_repeated=False, # So we don't have to manually add <emp> at each repeating char
+                              ctc_merge_repeated=False, # So we don't have to manually add <emp> at each repeating char
                               ignore_longer_outputs_than_inputs=True) # predicted = batch x time x feat_dim
-        loss = tf.reduce_mean(loss)
+        loss_mean = tf.reduce_mean(loss)
         wavenet_weights = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='wavenet')
-        grads_vars = opt.compute_gradients(loss, wavenet_weights)
+        grads_vars = opt.compute_gradients(loss_mean, wavenet_weights)
 
         return grads_vars, loss, wavenet_out
 
@@ -101,6 +106,6 @@ def calc_accuracy(labels, wavenet_out, seq_len ):
     d = p - y
     accuracy = (1 - tf.count_nonzero(d, dtype=tf.int32) / tf.size(y)) * 100
 
-    return accuracy
+    return accuracy, predicted_out
 if __name__ == '__main__':
     main()
