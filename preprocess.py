aa1ac1cf4f64373c30f31889df428eefe914a0cb
pythonpsycho1337 <Thomas_pierre_elis@hotmail.com>
2018-06-03 14:10:10 +0200

    Cleaned up preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,7 @@
 import numpy as np
 import pandas as pd
-import glob
-import csv
-import librosa
-import soundfile
 import dataloader as data
-import os
-import subprocess
-
-__author__ = 'namju.kim@kakaobrain.com'
+import glob,csv,librosa,soundfile,os,nltk
 
 
 # data path
@@ -129,74 +122,6 @@ def process_libri(csv_file, category):
 
             # save mfcc
             #np.save(target_filename, mfcc, allow_pickle=False)
-#
-# process TEDLIUM corpus
-#
-def convert_sph( sph, wav ):
-    """Convert an sph file into wav format for further processing"""
-    command = [
-        'sox','-t','sph', sph, '-b','16','-t','wav', wav
-    ]
-    subprocess.check_call( command ) # Did you install sox (apt-get install sox)
-
-def process_ted(csv_file, category):
-
-    parent_path = _data_path + 'TEDLIUM_release2/' + category + '/'
-    labels, wave_files, offsets, durs = [], [], [], []
-
-    # create csv writer
-    writer = csv.writer(csv_file, delimiter=',')
-
-    # read STM file list
-    stm_list = glob.glob(parent_path + 'stm/*')
-    for stm in stm_list:
-        with open(stm, 'rt') as f:
-            records = f.readlines()
-            for record in records:
-                field = record.split()
-
-                # wave file name
-                wave_file = parent_path + 'sph/%s.sph.wav' % field[0]
-                wave_files.append(wave_file)
-
-                # label index
-                labels.append(data.str2index(' '.join(field[6:])))
-
-                # start, end info
-                start, end = float(field[3]), float(field[4])
-                offsets.append(start)
-                durs.append(end - start)
-
-    # save results
-    for i, (wave_file, label, offset, dur) in enumerate(zip(wave_files, labels, offsets, durs)):
-        fn = "%s-%.2f" % (wave_file.split('/')[-1], offset)
-        target_filename = 'asset/data/preprocess/mfcc/' + fn + '.npy'
-        if os.path.exists( target_filename ):
-            continue
-        # print info
-        print("TEDLIUM corpus preprocessing (%d / %d) - '%s-%.2f]" % (i, len(wave_files), wave_file, offset))
-        # load wave file
-        if not os.path.exists( wave_file ):
-            sph_file = wave_file.rsplit('.',1)[0]
-            if os.path.exists( sph_file ):
-                convert_sph( sph_file, wave_file )
-            else:
-                raise RuntimeError("Missing sph file from TedLium corpus at %s"%(sph_file))
-        wave, sr = librosa.load(wave_file, mono=True, sr=None, offset=offset, duration=dur)
-
-        # get mfcc feature
-        mfcc = librosa.feature.mfcc(wave, sr=16000)
-
-        # save result ( exclude small mfcc data to prevent ctc loss )
-        if len(label) < mfcc.shape[1]:
-            # filename
-
-            # save meta info
-            writer.writerow([fn] + label)
-
-            # save mfcc
-            np.save(target_filename, mfcc, allow_pickle=False)
-
 
 #
 # Create directories
@@ -209,55 +134,8 @@ if not os.path.exists('asset/data/preprocess/mfcc'):
     os.makedirs('asset/data/preprocess/mfcc')
 
 
-#
-# Run pre-processing for training
-#
-
-# LibriSpeech corpus for training
-csv_f = open('asset/data/preprocess/meta/train.csv', 'w')
-process_libri(csv_f, 'dev-clean')
-csv_f.close()
-
-# # VCTK corpus
-# csv_f = open('asset/data/preprocess/meta/train.csv', 'w')
-# process_vctk(csv_f)
-# csv_f.close()
-#
-# # LibriSpeech corpus for train
-# csv_f = open('asset/data/preprocess/meta/train.csv', 'a+')
-# process_libri(csv_f, 'train-clean-360')
-# csv_f.close()
-#
-# # TEDLIUM corpus for train
-# csv_f = open('asset/data/preprocess/meta/train.csv', 'a+')
-# process_ted(csv_f, 'train')
-# csv_f.close()
-
-#
-# Run pre-processing for validation
-#
-
-# LibriSpeech corpus for training
-# csv_f = open('asset/data/preprocess/meta/valid.csv', 'w')
-# process_libri(csv_f, 'dev-clean')
-# csv_f.close()
-
-# # TEDLIUM corpus for valid
-# csv_f = open('asset/data/preprocess/meta/valid.csv', 'a+')
-# process_ted(csv_f, 'dev')
-# csv_f.close()
-
-#
-# Run pre-processing for testing
-#
-
-# # LibriSpeech corpus for test
-# csv_f = open('asset/data/preprocess/meta/test.csv', 'w')
-# process_libri(csv_f, 'test-clean')
-# csv_f.close()
-#
-# # TEDLIUM corpus for test
-# csv_f = open('asset/data/preprocess/meta/test.csv', 'a+')
-# process_ted(csv_f, 'test')
-# csv_f.close()
-
+if __name__ == "__main__":
+    # Run pre-processing for training
+    #csv_f = open('asset/data/preprocess/meta/train.csv', 'w')
+    #process_libri(csv_f, 'dev-clean')
+    #csv_f.close()
