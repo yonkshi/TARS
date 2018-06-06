d46072ada90fc8169c635408621f15df697ddaa1
yonk <nopctoday@gmail.com>
2018-06-06 18:17:08 +0200

    phoneme preprocessing

--- preprocess.py
+++ preprocess.py
@@ -189,7 +189,7 @@ def process_libri(csv_file, category):
             writer.writerow([fn] + label)
 
             # save mfcc
-            # np.save(target_filename, mfcc, allow_pickle=False)
+            np.save(target_filename, mfcc, allow_pickle=False)
 
 
 def getPhonemeIntMaps():
@@ -294,7 +294,7 @@ if __name__ == "__main__":
     #print(getWord(getPhonemes(nltk.corpus.cmudict.dict(),'campaign')))
 
     # Run pre-processing for training
-    csv_f = open('asset/data/preprocess/meta/augmented.csv', 'w')
+    csv_f = open('asset/data/preprocess/meta/phoneme.csv', 'w')
     #process_libri(csv_f, 'dev-clean')
     process_libri(csv_f, 'train-clean-100')
     # process_vctk(csv_f) #uncomment and comment out libri to switch to VCTK
